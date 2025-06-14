fd97eba4c43b4952173514fdc0bc8929bf8cfcb2
Carlos Campos <carlosalbertode.campos@gmail.com>
2025-06-14 17:42:19 -0300

    feat: Inclusão das Entidades

--- src/CreditoTrabalhador.Domain.Contracts/Entities/Usuario.cs
+++ src/CreditoTrabalhador.Domain.Contracts/Entities/Usuario.cs
@@ -5,10 +5,10 @@
 
         public Guid Id { get;  set; } = Guid.NewGuid();
 
-        public string Name { get;  set; } = string.Empty;
+        public string Nome { get;  set; } = string.Empty;
 
         public string Email { get;  set; } = string.Empty;
 
-        public string Password { get;  set; } = string.Empty;
+        public string Senha { get;  set; } = string.Empty;
     }
 }
